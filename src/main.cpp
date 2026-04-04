d840eeca77cf415050186cbc011b958cd261c948
Bluefairy-ljy <3099568863@qq.com>
2026-04-05 02:23:11 +0800

    Add serial sender code.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+#include "rm_deploy_stream/serial_sender_node.h"
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "rm_deploy_stream");
+  ros::NodeHandle nh;
+  rm_deploy_stream::SerialSenderNode serial_sender;
+  ros::AsyncSpinner spinner(3);
+  spinner.start();
+  ROS_INFO("All nodes started, spinning...");
+  ros::waitForShutdown();
+  return 0;
+}
